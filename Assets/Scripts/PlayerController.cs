db6d43932e6c71f57e596277026a11a3f09e18ca
Krzysztof Buzar <kbuzar@gmail.com>
2022-03-05 18:43:46 +0100

    Camera and shoting update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,29 @@ public class PlayerController : MonoBehaviour
     public Camera sceneCamera;
     public float playerSpeed;
     public Rigidbody2D rb;
-    public Weapon weapon;
+
+    public Weapon weapon_front_centre;
+    public Weapon weapon_front_left_1;
+    public Weapon weapon_front_left_2;
+    public Weapon weapon_front_left_3;
+    public Weapon weapon_front_right_1;
+    public Weapon weapon_front_right_2;
+    public Weapon weapon_front_right_3;
+
+    public Weapon weapon_front_left_1_angled;
+    public Weapon weapon_front_left_2_angled;
+    public Weapon weapon_front_left_3_angled;
+    public Weapon weapon_front_right_1_angled;
+    public Weapon weapon_front_right_2_angled;
+    public Weapon weapon_front_right_3_angled;
+
+    public Weapon weapon_back_centre;
+    public Weapon weapon_back_left_1;
+    public Weapon weapon_back_left_2;
+    public Weapon weapon_back_right_1;
+    public Weapon weapon_back_right_2;
+
+
     private Vector2 moveDirection;
     private Vector2 mousePosotion;
 
@@ -30,8 +52,28 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            weapon.Fire(); 
-        }
+
+        weapon_front_centre.Fire();
+        weapon_front_left_1.Fire();
+        weapon_front_left_2.Fire();
+        weapon_front_left_3.Fire();
+        weapon_front_right_1.Fire();
+        weapon_front_right_2.Fire();
+        weapon_front_right_3.Fire();
+
+        weapon_front_left_1_angled.Fire();
+        weapon_front_left_2_angled.Fire();
+        weapon_front_left_3_angled.Fire();
+        weapon_front_right_1_angled.Fire();
+        weapon_front_right_2_angled.Fire();
+        weapon_front_right_3_angled.Fire();
+
+        weapon_back_centre.Fire();
+        weapon_back_left_1.Fire();
+        weapon_back_left_2.Fire();
+        weapon_back_right_1.Fire();
+        weapon_back_right_2.Fire();
+}
         //mousebutton 1 ROCKETS
         //space Booster
         //E Barrelroll!
